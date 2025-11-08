bf5f5d61b30e2a1d10b2c3f44d9a7c2ec7944f6a
Anurag <anuragmishra11904@gmail.com>
2025-11-08 11:54:26 +0530

    practice

--- collage_Assignment/list3Nov.py
+++ collage_Assignment/list3Nov.py
@@ -16,7 +16,117 @@ Third_list.remove(4)#first occurnence remove
 print(Third_list)'''
 
 # Append and extend
-list=["Lali","Bhai"]
-# Third_list.append(list) #list me list aa jaygi
-Third_list.extend(list)# elements ki tarah na ki list ki tarah
-print(Third_list)
+# list=["Lali","Bhai"]
+# # Third_list.append(list) #list me list aa jaygi
+# Third_list.extend(list)# elements ki tarah na ki list ki tarah
+# print(Third_list)
+
+# NOV 6 
+#sorted()//copy nabata hai assign karna padega
+# sort()//usi list me sort kardega
+list=["banana","apple","mango","kiwi","graphes"]
+num=[22,33,22,44,55,11]
+# print(list[0])
+# print(num[0])
+# print(list[-1])#//end wala print hoga
+# print(list[-2])
+# print(list)
+
+# for i in list:
+#     print(i)
+
+# length=len(list)
+# print(length)
+    
+# list.append("papaya")
+# print(list)
+
+# for i in list:
+#     print("I like "+ i)
+
+# list.insert(1,"Black berry")
+# print(list)
+
+# list.remove("Black berry")
+# print(list)
+
+# list.pop()
+# print(list)# last value hatt jayga
+
+# del list[-1]
+# print(list)
+
+# a=[1,2]
+# b=[3,4]
+# print(a+b)
+
+# if "apple" in list:
+#     print("yes")
+
+# if "apple" not in list: check present or not in the given list
+#     print("NO")
+# sum=0
+# for i in num:
+#     sum+=i
+# print(sum)
+
+# avg=sum/len(num)
+# print(avg)
+
+# max=num[0]
+# for i in num:
+#     if(i>max):
+#         max=i
+# print(max)
+
+# min max
+
+print(max(num))
+print(min(num))
+# count=0
+# for i in num:
+#     if(i ==22):
+#         count=count+1
+
+# print(count)
+# print(num.count(22))
+
+# character=['x','y','x']
+# element='y'
+# for i in range(len(character)):
+#     if(character[i]==element):
+#         print(i)
+
+# num2=[1,1,2,2,3,3,4,4]
+# for i in num2:
+#     if(i==3):
+#         print(num2.index(i))
+#         break
+
+# Reverse the list
+list=["banana","apple","mango","kiwi","graphes"]
+
+# list.reverse()
+# print(list)
+        
+# # UpperCase every element of the list
+# list2=[]
+# for i in list:
+#     list2.append( i.upper())
+
+
+# print(list2)
+
+# alternate way 
+
+# for i in range(len(list)):
+#     list[i]= list[i].upper()
+
+
+# print(list)
+
+# print(num.index(22))
+
+
+
+# print(num[0:4])(lst)
