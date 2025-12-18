6499f26c0553341c84e641f023219423c611479e
Anurag <anuragmishra11904@gmail.com>
2025-12-18 12:15:23 +0530

    Codeforces Sheet 6

---
+++ Codeforces/sheet6/Prime_Checking.py
@@ -0,0 +1,22 @@
+# Prime checking
+
+def Prime(n):
+    if n<=1:
+        return False
+    if(n==2):
+        return True
+    if(n%2==0):
+        return False
+    for i in range(3,int(n**0.5)+1,2):
+            if(n%i==0):
+                 return False
+          
+    return True           
+            
+n=int(input())
+
+result=Prime(n)
+if result:
+     print("YES")
+else:
+     print("NO")
